dd4cc2464b92f5821c202f1e5f5dbadf146dc228
Tommaso Fontana <zommy21@hotmail.it>
2019-02-04 18:06:48 +0100

    up

---
+++ chroot-install.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+##########################################################
+# Script to be run inside the chroot during the installation
+##########################################################
+# Helper functions
+##########################################################
+function pause {
+               read -rsp $'Press enter to continue...\n'
+           }
+
+##########################################################
+# Configurations 
+##########################################################
+USER_NAME="userino"
+PASSWD="testerino"
+
+ROOT_PASSWD=PASSWD
+
+##########################################################
+
+# Add the networkmanager start on startup
+systemctl enable NetworkManager
+
+##########################################################
+# Bootloader Installation
+##########################################################
+# Silently install Grub
+pacman --noconfirm -S grub efibootmgr
+
+pause
+# Install Grub
+grub-install --target=i386-pc $DISK
+# Make the configs
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Install microcode
+# For Intel
+pacman -S intel-ucode
+# For Amd
+# pacstrap /mnt amd-ucode
+# Update Grub to get the microcode working
+grub-mkconfig -o /boot/grub/grub.cfg
+
+pause
+
+##########################################################
+# System configuration
+##########################################################
+# Set Time Zone
+ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+# Generate the file /etc/adjtime
+hwclock --systohc
+# Set the configuration of the localization
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+# Generate the locale
+locale-gen
+# Set the Locale of the system
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+# Set the Kayboard layout of the system
+echo "KEYMAP=us-latin1" >> /etc/vconsole.conf
+# Set hostname
+echo $HOSTNAME >> /etc/hostname
+# Complete the hosts file
+echo "127.0.0.1     $HOSTNAME" >> /etc/hostname
+echo "::1           $HOSTNAME" >> /etc/hostname
+echo "127.0.0.1     $HOSTNAME.locadomain $HOSTNAME" >> /etc/hostname
+# OPTIONAL
+# Generate RamDisk image using the linux preset
+#mkinitcpio -p linux
+# END OPTIONAL
+# Set Root Password
+echo "$ROOT_PASSWD" | passwd --stdin
+# Add the user
+adduser "$USER"
+# Set the password of the user
+echo "$PASSWD" | passwd "$USER" --stdin
+
+pause
+# Silently update the System 
+pacman --noconfirm -Syu
+
+pause
+# Remove system beeps
+rmmod pcspkr
+echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+##########################################################
+# Budgie Installation
+##########################################################
+# Install xorg
+pacman --noconfirm -Syu xorg-server xorg-xinit
+# Install Budgie
+pacman --noconfirm -Syu budgie-desktop-git
+# Set it as the defualt display manager
+printf "export XDG_CURRENT_DESKTOP=Budgie:GNOME\nexec budgie-desktop" >> /home/$USER/.xinitrc
---
+++ install.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+##########################################################
+#
+#   Arch Installation Script
+#     For Wm
+#
+#
+##########################################################
+# Helper functions
+##########################################################
+function pause {
+               read -rsp $'Press enter to continue...\n'
+           }
+
+##########################################################
+# Configurations 
+##########################################################
+
+HOSTNAME="$USER-pc"
+
+SWAP_DIM="4G"
+
+#SSID="Wifierino"
+#WIFI_PASS="WifiPasswarderino"
+
+##########################################################
+# Wifi Setup
+##########################################################
+
+# Get the interface name
+#INTERFACE=`iwconfig 2> /dev/null | grep -sho "wlp[^ ]*"`
+# Turn up the interface
+#ip link set $INTERFACE up
+# Connect to the wifi
+#wpa_supplicant -B -i interfaccia -c <(wpa_passphrase $SSID $WIFI_PASS)
+# Get an Ip from the DHCP
+#dhcpcd $INTERFACE
+
+##########################################################
+# General System Setup
+##########################################################
+# Set the us keyboard layout, it for italian
+loadkeys us
+# Update the system clock
+timedatectl set-ntp true
+##########################################################
+# Disk Setup (FOR VM)
+##########################################################
+#   Device  Size    Type
+# /dev/sda1 200M    Ext4
+# /dev/sda3  ~      Ext4
+##########################################################
+# Get the path to the first disk
+DISK=`fdisk -l | grep -ho /dev/sd[a-z] | head -n 1`
+
+# Remove all the partitions
+(   
+    # Delete partitions, 4 since its the non extended max
+    printf "d\n"; # Delete the previous partitions
+    printf "\n";  # Delete the previous partitions
+    printf "d\n"; # Delete the previous partitions
+    printf "\n";  # Delete the previous partitions
+    printf "d\n"; # Delete the previous partitions
+    printf "\n";  # Delete the previous partitions
+    printf "d\n"; # Delete the previous partitions
+    printf "\n";  # Delete the previous partitions
+    # Save and exit
+    printf "w\n"; # The rest of the disk
+    printf "Y\n" # 
+) | fdisk $DISK
+
+fdisk -l
+
+pause
+# Create the new partitions
+(   
+    # Delete partitions, 4 since its the non extended max
+    # Create the /mnt/boot partition
+    printf "n\n"; # new partition
+    printf "p\n"; # Primary partition
+    printf "1\n"; # Partition number
+    printf "\n"; # Start of the partition default
+    printf "+200M\n"; # 
+    printf "Y\n"; # 
+    # Create the Main partition
+    printf "n\n"; # new partition
+    printf "p\n"; # Primary partition
+    printf "2\n"; # Partition number
+    printf "\n"; # Start of the partition default
+    printf "\n"; # The rest of the disk
+    printf "Y\n"; # 
+    # Save and exit
+    printf "w\n"; # The rest of the disk
+    printf "Y\n" # 
+) | fdisk $DISK
+
+fdisk -l
+
+pause
+
+# Create the variables for the paths
+BOOT_PARTITION="$DISK"
+BOOT_PARTITION+="1"
+printf "Y\n" | mkfs.ext4 $BOOT_PARTITION
+# Mount the Boot partition
+mount $BOOT_PARTITION /boot
+# Convert the main partition to ext4
+MAIN_PARTITION="$DISK"
+MAIN_PARTITION+="2"
+printf "Y\n" | mkfs.ext4 $MAIN_PARTITION
+# Mount the Main partition
+mount $MAIN_PARTITION /mnt
+
+pause
+##########################################################
+# Swap File
+##########################################################
+# Allocate the space for the file
+fallocate -l $SWAP_DIM /mnt/swapfile
+# Set the minimum needed permission
+chmod 600 /mnt/swapfile
+# Format it to swap
+mkswap /mnt/swapfile
+# Activate the swap
+swapon /mnt/swapfile
+# Set the configuration if partition
+# echo "/mnt/swapfile non swap defaults 0 0" >> /etc/fstab
+
+pause
+##########################################################
+# System Installation
+##########################################################
+# Install the base system and the base development tools
+pacstrap /mnt base base-devel tmux vim nano networkmanager
+
+pause
+# Generate the Fstab file
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Download the script to be executed during the chroot
+wget URL -o /tmp/chroot-install.sh
+
+# Change the root to the disk
+arch-chroot /mnt bash /tmp/chroot-install.sh
+
+rm /tmp/chroot-install.sh
